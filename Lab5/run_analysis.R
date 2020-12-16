cb3ccf336a858afbe3cfd7901a8d0515130671e0
anya3121 <52558307+anya3121@users.noreply.github.com>
2020-12-16 13:12:31 +0200

    Update run_analysis.R

--- Lab5/run_analysis.R
+++ Lab5/run_analysis.R
@@ -2,6 +2,7 @@
 
 URL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(URL, destfile = paste0(getwd(), '/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'), method = "curl")
+no_zip<- unzip("getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",exdir = "data")
 
 x_train<-read.table('data/UCI HAR Dataset/train/X_train.txt')
 names(x_train) <- features
